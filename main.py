c1502ca07a7558d9e4f3f53415aa73b93c96bb24
Rishi Shanthosh <>
2021-06-01 22:59:50 +0000

    Adventure Page #1
    I am using a grid format to position the buttons and questions

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class GameStarter:
         self.entry_box.place(x=160, y=245)
 
         # Create a Button
-        self.continue_button = Button(self.game_frame, text="Enter", bg="skyblue", font=("Roman"), command=self.name_collection)
+        self.continue_button = Button(self.game_frame, text="Enter", bg="grey70", font=("Roman"), command=self.name_collection)
         self.continue_button.place(x=370, y=240)
 
     # When user types name it goes here
@@ -46,28 +46,31 @@ class Game:
   def __init__(self, parent):
 
     background_color = "Grey30"
-    self.game_frame = Frame(parent, bg = background_color, padx=245, pady=65)
+
+    self.game_frame = Frame(parent, bg = background_color, padx=91, pady=4)
     self.game_frame.grid()
 
-    self.question_label = Label(self.game_frame, font=("Tw Cen MT", "16"), bg=background_color)
+    self.question_label = Label(self.game_frame, text="In a cabin somewhere in the woods...", font=("Roman", "12"), background="grey70",  padx=50, pady=35)
     self.question_label.grid(row=1, padx=10, pady=10)
 
     self.var1 = IntVar()
 
-    self.rb1 = Radiobutton(self.game_frame, text="Get out of bed", font=("Skia","12"), bg=background_color, value=1, padx=10, pady=10, variable=self.var1, indicator = 0, background = "skyblue")
+    #Button 1
+    self.rb1 = Radiobutton(self.game_frame, text="Get out of bed", font=("Skia","12"), bg=background_color, value=1, padx=10, pady=10, variable=self.var1, indicator = 0, background = "grey70")
     self.rb1.grid(row=2, sticky=W)
 
-    self.rb2 = Radiobutton(self.game_frame, text="Sleep in for five more minutes", font=("Roman","12"), bg=background_color, value=1, padx=10, pady=10, variable=self.var1, indicator = 0, background = "skyblue")
+    #Button 2
+    self.rb2 = Radiobutton(self.game_frame, text="Sleep in for five more minutes", font=("Roman","12"), bg=background_color, value=2, padx=10, pady=10, variable=self.var1, indicator = 0, background = "grey70")
     self.rb2.grid(row=3, sticky=W)
 
-    self.rb3 = Radiobutton(self.game_frame, font=("Helvetica","12"), bg=background_color, value=1, padx=10, pady=10, variable=self.var1, indicator = 0, background = "skyblue")
-    self.rb1.grid(row=4, sticky=W)
+    self.rb3 = Radiobutton(self.game_frame, text="Pretend to be dead", font=("Helvetica","12"), bg=background_color, value=3, padx=10, pady=10, variable=self.var1, indicator = 0, background = "grey70")
+    self.rb3.grid(row=4, sticky=W)
 
-    self.game_instance = Button(self.game_frame, text="Confirm", font=("Helvetica", "13", "bold"), bg="whitesmoke")
-    self.game_instance.grid(row=8, padx=5, pady=1)
+    self.game_instance = Button(self.game_frame, text="Confirm", font=("Helvetica", "13", "bold"), bg="grey70")
+    self.game_instance.grid(row=5, padx=10, pady=10)
 
-    self.score_label = Label(self.game_frame, text="SCORE", font=("Tw Cen MT", "16"), bg=background_color)
-    self.score_label.grid(row=8, padx=10, pady=1)
+
+    
 
 
 # Start of program
